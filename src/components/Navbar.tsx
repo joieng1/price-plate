45390ae07f6a8970bc1bbcbe3e715cda80d526d9
joieng1 <johncieng1@gmail.com>
2024-05-23 00:39:27 -0700

    add logo to navbar and links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,24 +3,55 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-// import {}
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center bg-[#283D3B] h-[100px]">
-      <div className="item-center align-items-center px-5">
+    <nav className="flex items-center bg-[#283D3B] h-[120px]">
+      <Link
+        className="mx-auto absolute left-1/2 transform -translate-x-1/2"
+        href="/home"
+      >
+        <Image
+          className=""
+          src="/logo.png"
+          alt="Logo"
+          width={100}
+          height={100}
+        />
+      </Link>
+      <div className="item-center justify center align-items-center px-5">
         <DropdownMenu>
           <DropdownMenuTrigger>
-          <svg className="bg-[#9DB4AB] text-white rounded-md p-1" width="65" height="65" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1.5 3C1.22386 3 1 3.22386 1 3.5C1 3.77614 1.22386 4 1.5 4H13.5C13.7761 4 14 3.77614 14 3.5C14 3.22386 13.7761 3 13.5 3H1.5ZM1 7.5C1 7.22386 1.22386 7 1.5 7H13.5C13.7761 7 14 7.22386 14 7.5C14 7.77614 13.7761 8 13.5 8H1.5C1.22386 8 1 7.77614 1 7.5ZM1 11.5C1 11.2239 1.22386 11 1.5 11H13.5C13.7761 11 14 11.2239 14 11.5C14 11.7761 13.7761 12 13.5 12H1.5C1.22386 12 1 11.7761 1 11.5Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path></svg>
+            <svg
+              className="bg-[#9DB4AB] text-white rounded-md p-1"
+              width="65"
+              height="65"
+              viewBox="0 0 15 15"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M1.5 3C1.22386 3 1 3.22386 1 3.5C1 3.77614 1.22386 4 1.5 4H13.5C13.7761 4 14 3.77614 14 3.5C14 3.22386 13.7761 3 13.5 3H1.5ZM1 7.5C1 7.22386 1.22386 7 1.5 7H13.5C13.7761 7 14 7.22386 14 7.5C14 7.77614 13.7761 8 13.5 8H1.5C1.22386 8 1 7.77614 1 7.5ZM1 11.5C1 11.2239 1.22386 11 1.5 11H13.5C13.7761 11 14 11.2239 14 11.5C14 11.7761 13.7761 12 13.5 12H1.5C1.22386 12 1 11.7761 1 11.5Z"
+                fill="currentColor"
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
           </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuItem className="text-4xl text-white">Home</DropdownMenuItem>
-            <DropdownMenuItem className="text-4xl text-white">Recipes</DropdownMenuItem>
-            <DropdownMenuItem className="text-4xl text-white">Ingredients</DropdownMenuItem>
+          <DropdownMenuContent className="mt-4">
+            <DropdownMenuItem className="text-4xl text-white">
+            <Link href="/home">Home</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="text-4xl text-white">
+              <Link href="/recipeCard">Recipes</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="text-4xl text-white">
+            <Link href="/ingredients">Ingredients</Link>
+            </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
