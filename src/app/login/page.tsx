d8263318b12085a9b278bbbca2758696fc983a2d
aarav27 <aaravsharma927@gmail.com>
2024-05-23 01:41:15 -0700

    Revert "Merge branch 'dev' into createRecipePage-aarav"
    This reverts commit 124d7efd85ac25aa1bcab2afca9f5359bf76c518, reversing
    changes made to 721d0cc59f8b537df15ed1110ff836048e3d24c9.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,43 +1,9 @@
-import React from 'react';
-import Link from 'next/link';
-import styles from './login.module.css';
-
-const LoginPage = () => {
-  return (
-    <>
-      <div className={styles.loginBackground}>
-
-      <div className={styles.logoContainer}>
-          <img src="/logo.png" alt="Logo" className={styles.logoImage} />
-        </div>
-
-        <div className="flex flex-col items-center">
-          <div className={styles.loginContainer}>
-            <label htmlFor="username" className="text-black">Username</label>
-            <input
-              type="text"
-              id="username"
-              className={styles.loginInput}    
-            />
-          </div>
-
-          <div className={styles.loginContainer}>
-            <label htmlFor="password" className="text-black">Password</label>
-            <input
-              type="password"
-              id="password"
-              className={styles.loginInput}
-            />
-          </div>
-          <a href="signup" className={styles.signupLink}>New to PricePlate? Click here to create an account</a>
-          <Link href="/home">
-            <button className={styles.loginButton}>Login</button>
-          </Link>
-        </div> 
-
-      </div>
-    </>
-  );
-};
-
-export default LoginPage;
+const loginPage = () => {
+    return (
+      <>
+        loginPage
+      </>
+    );
+  };
+  
+  export default loginPage;
