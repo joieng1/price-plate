54891d67a9a34d717634faeed60f6e3a556215b1
joieng1 <johncieng1@gmail.com>
2024-05-23 14:44:04 -0700

    re add logo to signup and login pages

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import Link from "next/link";
 import styles from "./login.module.css";
+import Image from "next/image";
 
 const LoginPage = () => {
   return (
     <>
       <div className={styles.loginBackground}>
+        <Link href="/" className={styles.logoContainer}>
+          <Image
+            src="/logo.png"
+            alt="Logo"
+            width={100}
+            height={100}
+            className={styles.logoImage}
+          />
+        </Link>
+
         <div className="flex flex-col items-center">
           <div className={styles.loginContainer}>
             <label htmlFor="username" className="text-black">
