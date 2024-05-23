db87dc5806cbcd0d83d51ae22e420885436bb8b1
Kaila Aquino <kailad.aquino@gmail.com>
2024-05-22 18:32:07 -0700

    5/22 added logo and separate css file

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,73 +1,43 @@
+import React from 'react';
 import Link from 'next/link';
-const loginPage = () => {
-    return (
-      <>
-        {/* loginPage */}
-         
-      {/* <div style={{ backgroundColor: '#FFFCF4' }} className="flex items-center justify-center h-screen">
-        <div style={{ backgroundColor: '#9DB4AB' }}className="bg-white p-8 rounded shadow-md">
-        <p className="text-gray-700">Username</p>
+import styles from './login.module.css';
+
+const LoginPage = () => {
+  return (
+    <>
+      <div className={styles.loginBackground}>
+
+      <div className={styles.logoContainer}>
+          <img src="/logo.png" alt="Logo" className={styles.logoImage} />
+        </div>
+
+        <div className="flex flex-col items-center">
+          <div className={styles.loginContainer}>
+            <label htmlFor="username" className="text-black">Username</label>
+            <input
+              type="text"
+              id="username"
+              className={styles.loginInput}    
+            />
+          </div>
+
+          <div className={styles.loginContainer}>
+            <label htmlFor="password" className="text-black">Password</label>
+            <input
+              type="password"
+              id="password"
+              className={styles.loginInput}
+            />
+          </div>
+          <a href="signup" className={styles.signupLink}>New to PricePlate? Click here to create an account</a>
+          <Link href="/home">
+            <button className={styles.loginButton}>Login</button>
+          </Link>
+        </div> 
+
       </div>
-      </div> */}
-      <div className="flex items-center justify-center h-screen" style={{ backgroundColor: '#FFFCF4' }}>
+    </>
+  );
+};
 
-      <div className="flex flex-col items-center">
-        <div style={{ 
-          backgroundColor: '#9DB4AB', 
-          position: 'relative', 
-          zIndex: 1, width: 400 }} 
-          className="p-4 rounded shadow-md mb-4">
-
-          <label htmlFor="username"
-           className="text-black">Username</label>
-          <input
-            type="text"
-            id="username"
-            className="w-full px-3 py-2 border border-gray-300 rounded bg-gray-100 text-black"    
-          />
-        </div>
-
-        <div style={{ 
-          backgroundColor: '#9DB4AB', 
-          position: 'relative', 
-          zIndex: 1, width: 400}} 
-          className="p-4 rounded shadow-md mb-4">
-
-        <label htmlFor="password" className="text-black">Password</label>
-          <input
-            type="password"
-            id="password"
-            className="w-full px-3 py-2 border border-gray-300 rounded bg-gray-100 text-black"
-          />
-        </div>
-        <a href="signup" className="text-blue-500 mt-2 inline-block">New to PricePlate? Click here to create an account</a>
-        <Link href="/home">
-        <button style={{ 
-          backgroundColor: '#283D3B', 
-          color: 'white', 
-          border: 'none', 
-          padding: '10px 20px',
-          borderRadius: '4px', 
-          cursor: 'pointer', 
-          display: 'flex', 
-          justifyContent: 'center', 
-          marginTop: '20px' ,
-          alignItems: 'center' }} 
-
-          className="rounded shadow-md mb-4">
-            
-          Login
-        </button>
-        </Link>
-
-      </div> 
-
-    </div>
-    
-        
-      </>
-    );
-  };
-
- export default loginPage;
-
+export default LoginPage;
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,115 +1,47 @@
+import React from 'react';
 import Link from 'next/link';
+import styles from './signup.module.css';
 
-const signUpPage = () => {
-    return (
-        <>
-
-        <div className="flex items-center justify-center h-screen" style={{ backgroundColor: '#FFFCF4' }}>
-  
-        <div className="flex flex-col items-center">
-          <div style={{ 
-            backgroundColor: '#9DB4AB', 
-            position: 'relative', 
-            zIndex: 1, width: 400 }} 
-            className="p-4 rounded shadow-md mb-4">
-  
-            <label htmlFor="firstname"
-             className="text-black">First Name</label>
-            <input
-              type="text"
-              id="firstname"
-              className="w-full px-3 py-2 border border-gray-300 rounded bg-gray-100 text-black"    
-            />
-          </div>
-
-          
-          <div style={{ 
-            backgroundColor: '#9DB4AB', 
-            position: 'relative', 
-            zIndex: 1, width: 400 }} 
-            className="p-4 rounded shadow-md mb-4">
-  
-            <label htmlFor="lastname"
-             className="text-black">Last Name</label>
-            <input
-              type="text"
-              id="Last name"
-              className="w-full px-3 py-2 border border-gray-300 rounded bg-gray-100 text-black"    
-            />
-          </div>
-
-          <div style={{ 
-            backgroundColor: '#9DB4AB', 
-            position: 'relative', 
-            zIndex: 1, width: 400 }} 
-            className="p-4 rounded shadow-md mb-4">
-  
-            <label htmlFor="email"
-             className="text-black">Email</label>
-            <input
-              type="text"
-              id="email"
-              className="w-full px-3 py-2 border border-gray-300 rounded bg-gray-100 text-black"    
-            />
-          </div>
-
-          <div style={{ 
-          backgroundColor: '#9DB4AB', 
-          position: 'relative', 
-          zIndex: 1, width: 400 }} 
-          className="p-4 rounded shadow-md mb-4">
-
-          <label htmlFor="username"
-           className="text-black">Username</label>
-          <input
-            type="text"
-            id="username"
-            className="w-full px-3 py-2 border border-gray-300 rounded bg-gray-100 text-black"    
-          />
-        </div>
-  
-          <div style={{ 
-            backgroundColor: '#9DB4AB', 
-            position: 'relative', 
-            zIndex: 1, width: 400}} 
-            className="p-4 rounded shadow-md mb-4">
-  
-          <label htmlFor="password" className="text-black">Password</label>
-            <input
-              type="password"
-              id="password"
-              className="w-full px-3 py-2 border border-gray-300 rounded bg-gray-100 text-black"
-            />
-          </div>
-          <a href="login" className="text-blue-500 mt-2 inline-block">Already have an account? Click here to log in</a>
-          <Link href="/home">
-          <button style={{ 
-            backgroundColor: '#283D3B', 
-            color: 'white', 
-            border: 'none', 
-            padding: '10px 20px',
-            borderRadius: '4px', 
-            cursor: 'pointer', 
-            display: 'flex', 
-            justifyContent: 'center', 
-            marginTop: '20px' ,
-            alignItems: 'center' }} 
-  
-            className="rounded shadow-md mb-4">
-              
-            Sign up
-          </button>
-          </Link>
-  
-        </div> 
-  
+const SignUpPage = () => {
+  return (
+    <div className={styles.signUpBackground}>
+      <div className={styles.logoContainer}>
+        <img src="/logo.png" alt="Logo" className={styles.logoImage} />
       </div>
-      
-          
-        </>
-      );
-    };
-  
-  
 
-  export default signUpPage;
+      <div className="flex flex-col items-center">
+        <div className={styles.signUpContainer}>
+          <label htmlFor="firstname" className="text-black">First Name</label>
+          <input type="text" id="firstname" className={styles.signUpInput} />
+        </div>
+
+        <div className={styles.signUpContainer}>
+          <label htmlFor="lastname" className="text-black">Last Name</label>
+          <input type="text" id="lastname" className={styles.signUpInput} />
+        </div>
+
+        <div className={styles.signUpContainer}>
+          <label htmlFor="email" className="text-black">Email</label>
+          <input type="text" id="email" className={styles.signUpInput} />
+        </div>
+
+        <div className={styles.signUpContainer}>
+          <label htmlFor="username" className="text-black">Username</label>
+          <input type="text" id="username" className={styles.signUpInput} />
+        </div>
+
+        <div className={styles.signUpContainer}>
+          <label htmlFor="password" className="text-black">Password</label>
+          <input type="password" id="password" className={styles.signUpInput} />
+        </div>
+
+        <a href="login" className={styles.linkBlue}>Already have an account? Click here to log in</a>
+        <Link href="/home">
+          <button className={styles.signUpButton}>Sign up</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
