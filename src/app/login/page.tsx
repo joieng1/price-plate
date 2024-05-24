a2a5eb9c9f7078cd89316edd4c5eb6df190ee23f
aarav27 <aaravsharma927@gmail.com>
2024-05-23 18:18:57 -0700

    fixing build issues

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 import React, {useState} from 'react';
+import Link from "next/link";
+import Image from "next/image"
 import { useRouter } from "next/navigation";
 import styles from './login.module.css';
 
