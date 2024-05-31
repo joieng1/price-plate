7c7213ade227783a64cdb79023802db6c1e75188
joieng1 <johncieng1@gmail.com>
2024-05-31 00:01:58 -0700

    revert previous change, update route to send recipe id to User

--- src/app/api/recipe/route.ts
+++ src/app/api/recipe/route.ts
@@ -1,31 +1,45 @@
-import { NextRequest, NextResponse} from "next/server"
-import connectDB from "@/database/db"
+import { NextRequest, NextResponse } from "next/server";
+import connectDB from "@/database/db";
+import Recipes from "@/database/recipeSchema";
 import Users from "@/database/userSchema";
 
 export async function POST(req: NextRequest) {
   await connectDB();
   try {
-    const {userID, recipeName, recipeIngredients, totalCost} = await req.json();
-    const newRecipe = {
+    const { userID, recipeName, recipeIngredients, totalCost } =
+      await req.json();
+    const newRecipe = await new Recipes({
+      userID,
       recipeName,
       recipeIngredients,
-      totalCost
-    };
+      totalCost,
+    }).save();
 
-    // upload recipe to user
+    // add the recipe's ID to the recipes array of the user object
     const updatedUser = await Users.findByIdAndUpdate(
       userID,
-      { $push: { recipes: newRecipe } },
+      { $push: { recipes: newRecipe._id } },
       { new: true }
     );
 
     if (!updatedUser) {
-      return NextResponse.json({message: "Failed to add"});
+      return NextResponse.json({ message: "Failed to add" });
     }
-
-    return NextResponse.json(newRecipe)
+    return NextResponse.json(newRecipe);
   } catch (error) {
     console.log(error);
     return NextResponse.json(error, { status: 400 });
+  }
+}
+
+// returns all recipes
+export async function GET(req: NextRequest, res: NextResponse) {
+  await connectDB();
+  try {
+    const recipes = await Recipes.find().exec();
+    return NextResponse.json(recipes);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(error);
   }
 }
