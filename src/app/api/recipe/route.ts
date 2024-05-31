59f1253ee1611c5ed6db3179fdd362a45fed7551
joieng1 <johncieng1@gmail.com>
2024-05-30 23:51:26 -0700

    update route file to upload to user instead of recipe collection

--- src/app/api/recipe/route.ts
+++ src/app/api/recipe/route.ts
@@ -1,33 +1,31 @@
 import { NextRequest, NextResponse} from "next/server"
 import connectDB from "@/database/db"
-import Recipes from "@/database/recipeSchema";
+import Users from "@/database/userSchema";
 
 export async function POST(req: NextRequest) {
   await connectDB();
   try {
     const {userID, recipeName, recipeIngredients, totalCost} = await req.json();
-    const newRecipe = await new Recipes({
-      userID,
+    const newRecipe = {
       recipeName,
       recipeIngredients,
       totalCost
-    })
-    await newRecipe.save();
+    };
+
+    // upload recipe to user
+    const updatedUser = await Users.findByIdAndUpdate(
+      userID,
+      { $push: { recipes: newRecipe } },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      return NextResponse.json({message: "Failed to add"});
+    }
+
     return NextResponse.json(newRecipe)
   } catch (error) {
     console.log(error);
     return NextResponse.json(error, { status: 400 });
-  }
-}
-
-// returns all recipes
-export async function GET(req: NextRequest, res: NextResponse) {
-  await connectDB();
-  try {
-    const recipes = await Recipes.find().exec();
-    return NextResponse.json(recipes);
-  } catch (error) {
-    console.log(error);
-    return NextResponse.json(error);
   }
 }
