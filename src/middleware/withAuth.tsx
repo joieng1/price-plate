3825bf6518f8af4508fccfbb0a5c5ed2d29c0408
aarav27 <aaravsharma927@gmail.com>
2024-06-03 22:30:09 -0700

    Created function to protect pages and backend to do so

--- src/middleware/withAuth.tsx
+++ src/middleware/withAuth.tsx
@@ -1,23 +1,58 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+
+const jwt = require('jsonwebtoken');
 const withAuth = (WrappedComponent: React.ComponentType) => {
     return (props: any) => {
         const [isAuthenticated, setIsAuthenticated] = useState(false);
         const router = useRouter();
 
         useEffect(() => {
-            const token = localStorage.getItem('token');
-            if (!token) {
-                router.push('/login');
-            } 
-            else {
-                setIsAuthenticated(true);
-            }
-        }, 
-        []);
+            
+            const protectPage = async () => {
+                const token = localStorage.getItem("jwtToken");
+                const userID = localStorage.getItem("userID");
+
+                if (!token || !userID) {
+                    router.push('/login');
+                } 
+
+                else {
+                    try {
+                        const response = await fetch('/api/protected', {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json',
+                                'Authorization': `Bearer ${token}`
+                            },
+                            body: JSON.stringify({userID})
+                        });
+
+                        const jsonData = await response.json()
+                        if (jsonData.message == "Page Authorized") {
+                            setIsAuthenticated(true);
+                        } 
+
+                        else {
+                            localStorage.clear()
+                            router.push('/login');
+                        }
+
+                    } catch (error) {
+                        console.error("Failed to validate token:", error);
+                        router.push('/login');
+                    }
+                }
+            };
+            protectPage();
+        }, [router]);
 
         if (!isAuthenticated) {
-            return null; 
+            return ( 
+                <div>
+                    Page Unavailable
+                </div>
+            )
         }
 
         return <WrappedComponent {...props} />;
