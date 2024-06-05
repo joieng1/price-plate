87ccfccd13d628644379419515ddd371d3af4558
joieng1 <johncieng1@gmail.com>
2024-06-04 23:00:33 -0700

    feat: implement delete button and delete function for ingredients

--- src/app/ingredients/page.tsx
+++ src/app/ingredients/page.tsx
@@ -1,8 +1,8 @@
-"use client"
+"use client";
 import React, { useState, useEffect, use } from "react";
 import styles from "./ingredients.module.css";
 import { Box, Button, List, ListItem, ListItemText } from "@mui/material";
-import Link from 'next/link';
+import Link from "next/link";
 import withAuth from "@/middleware/withAuth";
 
 interface Ingredient {
@@ -11,19 +11,63 @@ interface Ingredient {
   pricePerUnit: number;
 }
 
-function IngredientsList({ ingredientList }: { ingredientList: Ingredient[] }) {
+function IngredientsList({ ingredientList, refreshIngredients }: { ingredientList: Ingredient[], refreshIngredients: () => void }) {
+  //deletes ingredient
+  const handleClick = async (ingredientId: string) => {
+    const token = localStorage.getItem("jwtToken");
+    console.log("Attempting to delete ", ingredientId)
+    try {
+      const response = await fetch(`/api/ingredient/${ingredientId}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      alert("Deleted");
+      refreshIngredients();
+      // Handle success
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Delete failed");
+      // Handle error
+    }
+  }
+  
   return (
     <Box>
       <List>
         {ingredientList.map((ingredient, index) => (
           <ListItem key={index} className={styles.ingredient}>
-            <ListItemText className = {styles.ingredientfield} 
-              primary={`${ingredient.ingredientName}`} 
-              secondary={`$${ingredient.pricePerUnit} per ${ingredient.unitType}`}/>
+            <ListItemText
+              className={styles.ingredientfield}
+              primary={`${ingredient.ingredientName}`}
+              secondary={`$${ingredient.pricePerUnit} per ${ingredient.unitType}`}
+            />
             {/* <ListItemText className = {styles.ingredientfield} primary={`$${ingredient.unit_cost} per unit`} /> */}
-            <Link href="/ingredientCard">
-              <Button variant="contained" className = {styles.moreinfo} color = "success" >More Info</Button>
-            </Link>
+            <div className={styles.container}>
+              <Link href={`/ingredientCard/${ingredient._id}`}>
+                <Button
+                  variant="contained"
+                  className={styles.moreinfo}
+                  color="success"
+                >
+                  More Info
+                </Button>
+              </Link>
+              <Button
+                variant="contained"
+                className={styles.moreinfo}
+                color="error"
+                onClick={()=>handleClick(ingredient._id)}
+              >
+                Delete
+              </Button>
+            </div>
           </ListItem>
         ))}
       </List>
@@ -31,7 +75,13 @@ function IngredientsList({ ingredientList }: { ingredientList: Ingredient[] }) {
   );
 }
 
-const SearchBar = ({ onChange, value }: { onChange: (e: React.ChangeEvent<HTMLInputElement>) => void, value: string }) => {
+const SearchBar = ({
+  onChange,
+  value,
+}: {
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  value: string;
+}) => {
   return (
     <input
       type="search"
@@ -46,61 +96,66 @@ const SearchBar = ({ onChange, value }: { onChange: (e: React.ChangeEvent<HTMLIn
 function IngredientsPage() {
   const [ingredients, setIngredients] = useState<Ingredient[]>([]);
   const [searchInput, setSearchInput] = useState<string>("");
+  
+  const fetchIngredients = async () => {
+    try {
+      const token = localStorage.getItem("jwtToken");
+      const userID = localStorage.getItem("userID");
+
+      const response = await fetch(`/api/ingredient?userID=${userID}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch ingredients");
+      }
+
+      const data: Ingredient[] = await response.json();
+      setIngredients(data);
+    } catch (error) {
+      console.error("Error fetching ingredients:", error);
+    }
+  };
 
   useEffect(() => {
-    const fetchIngredients = async () => {
-      try{
-
-        const token = localStorage.getItem('jwtToken');
-        const userID = localStorage.getItem('userID');
-
-        const response = await fetch(`/api/ingredient?userID=${userID}`, {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token}`,
-          },
-        });
-
-        if(!response.ok){
-          throw new Error('Failed to fetch ingredients');
-        }
-
-        const data: Ingredient[] = await response.json();
-        setIngredients(data);
-      }
-      catch(error){
-        console.error('Error fetching ingredients:', error);
-      }
-    };
-
     fetchIngredients();
   }, []);
-
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchInput(e.target.value);
   };
 
   const filteredIngredients = ingredients.filter((ingredient) => {
-    return ingredient.ingredientName.toLowerCase().includes(searchInput.toLowerCase());
+    return ingredient.ingredientName
+      .toLowerCase()
+      .includes(searchInput.toLowerCase());
   });
 
   return (
     <div className={styles.ingredientsPage}>
       <div className={styles.ingredientBox}>
         <SearchBar onChange={handleChange} value={searchInput} />
-          {filteredIngredients.length === 0 ? (
-            <p>No ingredients found</p>
-              ) : (
-            <IngredientsList ingredientList={filteredIngredients} />
-          )}
+        {filteredIngredients.length === 0 ? (
+          <p>No ingredients found</p>
+        ) : (
+          <IngredientsList ingredientList={filteredIngredients} refreshIngredients={fetchIngredients}/>
+        )}
         <Link href="/createIngredient">
-          <Button variant="contained" className={styles.createbutton} color="success">Create New Ingredient</Button>
-        </Link> 
+          <Button
+            variant="contained"
+            className={styles.createbutton}
+            color="success"
+          >
+            Create New Ingredient
+          </Button>
+        </Link>
       </div>
     </div>
   );
 }
 
-export default withAuth(IngredientsPage)
+export default withAuth(IngredientsPage);
