07f0fe5ade555353fef0eb783b0393d38e4e3e18
joieng1 <johncieng1@gmail.com>
2024-06-05 18:31:48 -0700

    unittype enum

---
+++ src/types/unitTypes.ts
@@ -0,0 +1,13 @@
+export enum UnitTypeEnum {
+  KG = "kg",
+  LB = "lb",
+  OZ = "oz",
+  G = "g",
+  TSP = "tsp",
+  TBSP = "tbsp",
+  CUP = "cup",
+  QUART = "qt",
+  GAL = "gal",
+  FL_OZ="floz",
+  UNIT="unit"
+};
